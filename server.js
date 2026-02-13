0b70cf38be61465fa7bfb4c0bea35fdf05dc107f
Tony-123-a11y <ayushsinghatschool@gmail.com>
2026-02-13 11:07:57 +0530

    profilePicUpload

--- server.js
+++ server.js
@@ -29,9 +29,8 @@ app.use(
     credentials: true, // Required to allow cookies, including HttpOnly cookies
   }),
 );
-
-app.use(express.json());
-// main server file
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
